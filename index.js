1bbea6a044dcd993072cbc85383452ae5d7c73b1
Yashar Ghafarinejad <yasharg97@gmail.com>
2023-09-11 19:01:56 -0500

    'primer-commit'

---
+++ index.js
@@ -0,0 +1,6 @@
+// // const menu = document.getElementById('menu');
+// const hambuerguesa = document.getElementById('nav-icon');
+
+// hambuerguesa.addEventListener('click', () => {
+//     menu.classList.toggle('efecto')
+// });
